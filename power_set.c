c84c07ed0262810441e2a45b791b9333eb1bd115
ELamraniG <moha.mati@hotmail.com>
2025-08-15 14:51:52 +0100

    yessure

--- power_set.c
+++ power_set.c
@@ -1,7 +1,21 @@
 //Allowed functions: atoi, printf, fprintf, malloc, calloc, realloc, free, stdout,
 #include <stdio.h>
 #include <unistd.h>
-int main(char ac, char **av)
+
+void ft_power_set(int *nums,int pos,int size)
 {
 	
+}
+int main(char ac, char **av)
+{
+	if (ac <= 2)
+		return 0;
+	int nums[ac - 2];
+	int i = 2;
+	while (av[i])
+	{
+		nums[i - 2] = atoi(av[i]);
+		i++;
+	}
+	ft_power_set(nums,0,ac - 2);
 }
